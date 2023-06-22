2970b0287d5615ee69d4b10ee2da6688521b5355
SelomieK <guskinse@student.gu.se>
2023-06-22 22:50:45 +0200

    font adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,18 +7,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First Flutter',
       home: Scaffold(
-        appBar: AppBar(
-            title: const Text('Hello from Flutter'),
-            leading: const Icon(Icons.flutter_dash)),
-        body: const Center(
+        appBar: AppBar(title: const Text('Hello from Flutter')),
+        body: Center(
           child: Text(
-            'Hello Flutter!',
-            textDirection: TextDirection.ltr,
+            'Baby Flutter App Is Here:)',
+            style: GoogleFonts.acme(fontSize: 32),
           ),
         ),
       ),
